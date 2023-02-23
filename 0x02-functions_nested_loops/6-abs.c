cf13eff305da6da68157862467fce45b4657cd28
ibnshuaib06 <shafiuibnshuaib@gmail.com>
2023-02-22 16:40:51 -0800

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _abs - a function that computes the absolute value of an integer.
+ *
+ * @s: variable name
+ *
+ * Return: Always 0
+ */
+int _abs(int s)
+{
+	if (s >= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+
+	_putchar('\n');
+}
